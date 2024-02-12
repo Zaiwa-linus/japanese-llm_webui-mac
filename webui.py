5c62df470957fe81a9f3c120a6ed7483124333a7
Mitsuhiro <mitsuhiro.n.0919@gmail.com>
2024-02-12 12:56:24 +0900

    モデルの事前ロード機能の追加

--- webui.py
+++ webui.py
@@ -1,27 +1,36 @@
 import streamlit as st
-from Script import generate
+from Script import generate, models
 import mlx.core as mx
-from Script import models
+
+# モデルとトークナイザのロード状態を保持するためのセッション状態の初期化
+if 'model_loaded' not in st.session_state:
+    st.session_state['model_loaded'] = False
+
+# サイドバーにロードボタンの設定
+with st.sidebar:
+    gguf = 'mistral-7b-v0.1.Q8_0.gguf'
+    repo = 'TheBloke/Mistral-7B-v0.1-GGUF'
+    load_button = st.button("Load Model")
+    if load_button:
+        st.session_state['model_loaded'] = False  # ロード前にフラグをリセット
+        with st.spinner('Model loading...'):
+            mx.random.seed(100)  # 乱数生成器のシードを設定
+            st.session_state['model'], st.session_state['tokenizer'] = models.load(gguf, repo)  # モデルとトークナイザをロード
+            st.session_state['model_loaded'] = True
+            st.success('Model loaded successfully!')
 
 # UI要素の定義（ユーザー入力など）
-prompt = st.text_input("Prompt:", "ここにプロンプトを入力してください")
+prompt = st.text_input("Prompt:", "")
 max_tokens = st.slider("Max tokens:", min_value=1, max_value=1000, value=100)
-temp = st.slider("Temperature:", min_value=0.0, max_value=1.0, value=0)
+temp = st.slider("Temperature:", min_value=0.0, max_value=1.0, value=0.0)
 
-if st.button("Generate"):
-    gguf ='mistral-7b-v0.1.Q8_0.gguf'
-    repo = 'TheBloke/Mistral-7B-v0.1-GGUF'
-    prompt = 'Write a quicksort in Python'
-    max_token = 100
-
-    mx.random.seed(100)  # 乱数生成器のシードを設定
-    model, tokenizer = models.load(gguf, repo)  # モデルとトークナイザをロード
-
-    # tokens = generate.generate_tokens(model, tokenizer, prompt, max_tokens, temp)  # モデルとトークナイザは事前に定義されていると仮定
-    # generated_text = tokenizer.decode(tokens)
-    # st.text_area("Generated Text:", value=generated_text, height=300)
-
-    output_container = st.empty()  # 出力表示用のプレースホルダー
-    for generated_text in generate.generate_tokens_iter(model, tokenizer, prompt, max_tokens, temp):
-        # プレースホルダーの内容を更新
-        output_container.text_area("Generated Text:", value=generated_text, height=300)
+# Generateボタン（モデルがロードされている場合のみ有効）
+if st.session_state['model_loaded']:
+    if st.button("Generate"):
+        output_container = st.empty()  # 出力表示用のプレースホルダー
+        for generated_text in generate.generate_tokens_iter(st.session_state['model'], st.session_state['tokenizer'], prompt, max_tokens, temp):
+            # プレースホルダーの内容を更新
+            output_container.text_area("Generated Text:", value=generated_text, height=300)
+else:
+    # モデルがロードされていない場合、Generateボタンを無効化
+    st.write("Please load the model first using the 'Load Model' button on the sidebar.")
